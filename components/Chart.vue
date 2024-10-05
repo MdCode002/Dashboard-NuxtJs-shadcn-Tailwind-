8d9ee0508f934f28444942c1b7419fd0042bf454
Mouhamed Diouf <dioufmouhamed002@gmail.com>
2024-10-05 20:07:27 +0000

    change my chart to a external component and display it on the index

---
+++ components/Chart.vue
@@ -0,0 +1,69 @@
+<script setup>
+let props = defineProps(["currentCategory", "data"]);
+let data = ref(props.data);
+let currentCategory = props.currentCategory || "today";
+console.log(data);
+
+let categories = ref({
+  today: [
+    '00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00',
+    '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00',
+    '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00',
+  ],
+  week: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+  year: [
+    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December',
+  ],
+});
+
+const chartOptions = computed(() => {
+  return {
+    chart: {
+      type: 'line',
+    },
+    title: {
+      text: '',
+    },
+    xAxis: {
+      categories: categories.value[currentCategory.value], // Correction ici
+    },
+    yAxis: {
+      title: {
+        text: '',
+      },
+    },
+    plotOptions: {
+      line: {
+        marker: {
+          enabled: false,
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        enableMouseTracking: true,
+      },
+    },
+    series: [{
+      name: 'line',
+      linewidth: '4px',
+      data: data.value,
+      color: {
+        linearGradient: {},
+        stops: [
+          [0, 'rgba(252, 176, 69, 1)'],
+          [0.33, 'rgba(253, 29, 29, 1)'],
+          [0.66, 'rgba(131, 58, 180, 1)'],
+          [1, 'rgba(29, 217, 83, 1)'],
+        ],
+      },
+    }],
+  };
+});
+
+</script>
+<template>
+    <div>
+        <highchart v-if="data.length > 0" :options="chartOptions" />
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,3 @@
-<template>
-  <div class="grid gap-8">
-    <header class="flex item-start justify-between">
-      <div>
-        <p>Hi Mouhamed welcome back</p>
-        <h1>Dashboard</h1>
-      </div>
-      <div class="bg-neutral-200 w-[80px] h-[30px]"></div>
-    </header>
-    <main class="grid gap-4">
-      <Tabs :default-value="listTabs[0].value">
-        <TabsList class="w-[400px]">
-          <TabsTrigger
-            v-for="(listTab, index) in listTabs"
-            :key="index"
-            :value="listTab.value"
-            @click="setCategory(listTab.value)"
-          >
-            {{ listTab.label }}
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent
-          v-for="(listTab, index) in listTabs"
-          :key="index"
-          :value="listTab.value"
-        >
-          <highchart :options="chartOptions" />
-        </TabsContent>
-      </Tabs>
-    </main>
-    <footer>
-      <div class="flex items-center gap-2">
-        <div class="bg-neutral-200 h-[200px] w-full"></div>
-        <div class="bg-neutral-200 h-[200px] w-full"></div>
-        <div class="bg-neutral-200 h-[200px] w-full"></div>
-      </div>
-    </footer>
-  </div>
-</template>
 
 <script setup>
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
@@ -66,64 +27,9 @@ let data = ref([
   22.0, 17.8,
 ]);
 
-let categories = ref({
-  today: [
-    '00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00',
-    '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00',
-    '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00',
-  ],
-  week: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-  year: [
-    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
-    'September', 'October', 'November', 'December',
-  ],
-});
 
 let currentCategory = ref('today');
 
-const chartOptions = computed(() => {
-  return {
-    chart: {
-      type: 'line',
-    },
-    title: {
-      text: '',
-    },
-    xAxis: {
-      categories: categories.value[currentCategory.value], // Correction ici
-    },
-    yAxis: {
-      title: {
-        text: '',
-      },
-    },
-    plotOptions: {
-      line: {
-        marker: {
-          enabled: false,
-        },
-        dataLabels: {
-          enabled: false,
-        },
-        enableMouseTracking: true,
-      },
-    },
-    series: [{
-      name: 'line',
-      linewidth: '4px',
-      data: data.value,
-      color: {
-        linearGradient: {},
-        stops: [
-          [0, 'rgba(252, 176, 69, 1)'],
-          [0.33, 'rgba(253, 29, 29, 1)'],
-          [0.66, 'rgba(131, 58, 180, 1)'],
-          [1, 'rgba(29, 217, 83, 1)'],
-        ],
-      },
-    }],
-  };
-});
 
 function generateRandomData(number = 23) {
   let values = [];
@@ -157,3 +63,45 @@ onMounted(() => {
   generateRandomData();
 });
 </script>
+
+
+<template>
+  <div class="grid gap-8">
+    <header class="flex item-start justify-between">
+      <div>
+        <p>Hi Mouhamed welcome back</p>
+        <h1>Dashboard</h1>
+      </div>
+      <div class="bg-neutral-200 w-[80px] h-[30px]"></div>
+    </header>
+    <main class="grid gap-4">
+      <Tabs :default-value="listTabs[0].value">
+        <TabsList class="w-[400px]">
+          <TabsTrigger
+            v-for="(listTab, index) in listTabs"
+            :key="index"
+            :value="listTab.value"
+            @click="setCategory(listTab.value)"
+          >
+            {{ listTab.label }}
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent
+          v-for="(listTab, index) in listTabs"
+          :key="index"
+          :value="listTab.value"
+        >
+          <Chart :currentCategory="currentCategory" :data="data"/>
+        </TabsContent>
+      </Tabs>
+    </main>
+    <footer>
+      <div class="flex items-center gap-2">
+        <div class="bg-neutral-200 h-[200px] w-full"></div>
+        <div class="bg-neutral-200 h-[200px] w-full"></div>
+        <div class="bg-neutral-200 h-[200px] w-full"></div>
+      </div>
+    </footer>
+  </div>
+</template>
+
